e3ba03c783cbd40770871adf1090758684a3056a
Jaime Sansano Garcia <jasano2@hotmail.com>
2020-03-05 03:52:49 +0100

    Añadir clase de pruebas JUnit TestASumar en el carpeta test

---
+++ test/sumador/TestASumar.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package sumador;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Jay
+ *
+ */
+public class TestASumar {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
